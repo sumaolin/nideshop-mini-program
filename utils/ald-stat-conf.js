141aad55aa4afbeb163efa4e043fa94524f61613
sumaolin <sumaolin619@gmail.com>
2018-03-19 15:43:05 +0800

    feat:
    init ald sdk

---
+++ utils/ald-stat-conf.js
@@ -0,0 +1,5 @@
+exports.app_key = '055d69f43d275480c1c834266045a447' //请在此行填写从阿拉丁后台获取的appkey
+exports.getLocation = false //默认不获取用户坐标位置
+exports.appid = 'wxe6e993687ad74d1a' //用于用户登录、微信转发群信息、二维码等微信官方功能
+exports.appsecret = '87e975261b0f1f007d0da2d9a663e609' //用于用户登录、微信转发群信息、二维码等微信官方功能
+exports.defaultPath = 'pages/index/index' //小程序的默认首页, 用于分享时path为空时
