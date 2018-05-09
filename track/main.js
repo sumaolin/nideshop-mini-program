efdad8752482a94002dbdec7ad407afaf171e56c
sumaolin <sumaolin619@gmail.com>
2018-05-09 16:20:27 +0800

    feat:
    Page.js 事件 整合到 main 程序入口

--- track/main.js
+++ track/main.js
@@ -3,6 +3,8 @@ import kmConfig from './kmConfig'
 // KM-CONFIG SDK全局配置信息
 import KMC from './config'
 import wxu from './wxUtils'
+import _ from './utils'
+
 // km state 公用状态信息放着
 var KMS = {
   rq_c: 0
@@ -83,22 +85,89 @@ var KMS = {
     N(t)
   }
 
-  var preHandlePageOnLoad = function(opt) {
+  /**
+   * bind Page onLoad
+   * @param  {Object} t onLoad参数  可以在 onLoad 中获取打开当前页面所调用的 query 参数。
+   */
+  var preHandlePageOnLoad = function(t) {
     console.log(this['__route__'] + ' pre Handle Page OnLoad')
-    console.dir(opt)
+    // kmsrc 自定义参数kmsrc -> sr
+    var e = ''
+    try {
+      e = wx.getStorageSync(KMC.prefix + 'src')
+    } catch (t) {
+      e = ''
+    }
+    var app = getApp()
+
+    if (e) {
+      app[KMC.prefix + 'src'] = e
+    }
+    if (!_.isEmptyObject(t)) {
+      // t 为 Object 类型
+      if (typeof t.kmsrc != 'undefined') {
+        if (!e) {
+          try {
+            wx.setStorageSync(KMC.prefix + 'src', t.kmsrc)
+          } catch (t) {}
+          app[KMC.prefix + 'src'] = t.kmsrc
+        }
+      }
+      // refer share user
+      if (typeof t.refer_share_user != 'undefined') {
+        app.refer_share_user = t.refer_share_user
+      }
+      this[KMC.prefix + 'page_args'] = JSON.stringify(t)
+    }
+    // referer Path
+    var rPath = wx.getStorageSync(KMC.prefix + 'refererPath')
+    if (rPath) {
+      app[KMC.prefix + 'refererPath'] = rPath
+    }
+    // page_count
+    if (!app[KMC.prefix + 'page_count']) {
+      app[KMC.prefix + 'page_count'] = 1
+    } else {
+      app[KMC.prefix + 'page_count']++
+    }
+
+    wxu.sendPageTrack(app, this, 'load')
   }
 
-  var preHandlePageOnUnload = function(opt) {
+  /**
+   * Page onUnload
+   */
+  var preHandlePageOnUnload = function() {
     console.log(this['__route__'] + ' pre Handle Page OnUnload')
+    var app = getApp()
+    var path = this['__route__']
+    wx.setStorageSync(KMC.prefix + 'refererPath', path)
+    wxu.sendPageTrack(app, this, 'unload')
   }
-
-  var preHandlePageOnShow = function(opt) {
+  /**
+   * Page onShow
+   */
+  var preHandlePageOnShow = function(t, a) {
     console.log(this['__route__'] + ' pre Handle Page OnShow')
-    console.dir(opt)
+    console.dir(t)
+    var app = getApp()
+    var _this = this
+    _this[KMC.prefix + 'start_time'] = Date.now()
+    var rPath = wx.getStorageSync(KMC.prefix + 'refererPath')
+    if (rPath) {
+      app[KMC.prefix + 'refererPath'] = rPath
+    }
+    wxu.sendPageTrack(app, _this, 'show')
   }
-
-  var preHandlePageOnHide = function() {
+  /**
+   * Page onHide
+   */
+  var preHandlePageOnHide = function(t, a) {
     console.log(this['__route__'] + ' pre Handle Page OnHide')
+    var app = getApp()
+    var path = this['__route__']
+    wx.setStorageSync(KMC.prefix + 'refererPath', path)
+    wxu.sendPageTrack(app, this, 'hide')
   }
 
   var preHandlePageOnReachBottom = function() {
